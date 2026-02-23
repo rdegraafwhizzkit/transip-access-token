d56c1f004e294296ebb32d7df1070451ca05c1a4
Rutger de Graaf <rutger@whizzkit.nl>
2026-02-23 23:52:03 +0100

    Changed licensing setup

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="transip-access-token-python",
-    version="1.0.0",
-    author="Rutger de Graaf",
-    author_email="",
-    description="TransIP Access Token Python library",
+    name='transip-access-token-python',
+    version='1.0.0',
+    author='Rutger de Graaf',
+    author_email='',
+    description='TransIP Access Token Python library',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rdegraafwhizzkit/transip-access-token",
+    long_description_content_type='text/markdown',
+    url='https://github.com/rdegraafwhizzkit/transip-access-token',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',
     install_requires=[
         'requests',
         'cryptography'
-    ]
+    ],
+    license='MIT'
 )
